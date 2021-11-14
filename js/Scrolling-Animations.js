7ffeb47ddfde98d6d344670f054e5e87b5cdc5b9
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-11-14 17:37:33 +0700

    Update Scrolling-Animations.js

--- js/Scrolling-Animations.js
+++ js/Scrolling-Animations.js
@@ -17,6 +17,9 @@
     //     });
     // });
 
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 
 // ----------------------------------//
 //                                   //
