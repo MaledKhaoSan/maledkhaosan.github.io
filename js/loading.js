1a364408fa8157670ce1414bc5863ec136cffc1a
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-12-24 22:44:20 +0700

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,47 +1,26 @@
-	if(window.location.hash) {
-		// Fragment exists
-		var refresh = window.localStorage.getItem('refresh');
-		console.log("with Hash");
-		if (refresh===null){
-			window.location.reload();
-			window.localStorage.setItem('refresh', "1");
-		}
-		window.onbeforeunload = function () {
-			window.scrollTo(0, 0);
-			window.requestAnimationFrame();
-		}
-		$(window).bind('load', function() {
-			$('.overlay, body').addClass('loaded');
-			setTimeout(function() {
-				$('.overlay').css({'display':'none'})
-
-			}, 2900)
-		});
-
-	  } else {
-		// Fragment doesn't exist
-	  }
-
 
 $(document).ready(function() {
+	// Will remove overlay for users cannnot load properly.
+	setTimeout(function() {
+		$('.overlay, body').addClass('loaded');
+	}, 120000);
+
+	// Will wait for everything on the page to load.
 	$(window).bind('load', function() {
 		$('.overlay, body').addClass('loaded');
 		setTimeout(function() {
 			$('.overlay').css({'display':'none'})
-			if(!window.location.hash) {
-				window.location = window.location + '#Introduction';
-				window.scrollTo(0, 0);
-				window.requestAnimationFrame();
-				
-				var refresh = window.localStorage.getItem('refresh');
-				    console.log(refresh);
-				    if (refresh===null){
-					window.location.reload();
-					window.localStorage.setItem('refresh', "1");
-				    }
-			}
-
-		}, 2900)
-	});
+			
+		}, 120000)
+	})
 })
 
+
+
+
+window.onload = function() {
+	if(!window.location.hash) {
+		window.location = window.location + '#Introduction';
+		window.location.reload();
+	}
+}
