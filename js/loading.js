a2e1f44039daa1e38588304f0c3418323cea6d89
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-12-25 13:56:32 +0700

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -3,15 +3,17 @@ $(document).ready(function() {
 	// Will remove overlay for users cannnot load properly.
 	setTimeout(function() {
 		$('.overlay, body').addClass('loaded');
-	}, 120000);
+		window.location.reload();
+	}, 29000);
 
 	// Will wait for everything on the page to load.
 	$(window).bind('load', function() {
 		$('.overlay, body').addClass('loaded');
 		setTimeout(function() {
 			$('.overlay').css({'display':'none'})
+			window.location.reload();
 			
-		}, 120000)
+		}, 29000)
 	})
 })
 
