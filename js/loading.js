9f91f1425c5ebc69a428d4b8cb44f79fcf57d7da
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-12-25 15:41:18 +0700

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -13,9 +13,21 @@ $(document).ready(function() {
 			
 		}, 2900)
 	})
+	window.location.reload();
 })
 
 
+var refresh = window.localStorage.getItem('refresh');
+console.log(refresh);
+if (refresh===null){
+	window.location.reload();
+	window.localStorage.setItem('refresh', "1");
+}
+    
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+  window.requestAnimationFrame();
+}
 
 
 window.onload = function() {
