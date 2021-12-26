1c2edb5c221d37a3534a542e6778300ed8a06204
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-12-26 10:36:12 +0700

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -3,21 +3,19 @@ $(document).ready(function() {
 	// Will remove overlay for users cannnot load properly.
 	setTimeout(function() {
 		$('.overlay, body').addClass('loaded');
-	}, 2900);
+	}, 9900);
 
 	// Will wait for everything on the page to load.
 	$(window).bind('load', function() {
 		$('.overlay, body').addClass('loaded');
 		setTimeout(function() {
 			$('.overlay').css({'display':'none'})
-		}, 2900)
+		}, 9900)
 	})
 })
 
-
-
 window.onload = function() {
-	if(window.location.hash) {
+	if(!window.location.hash) {
 		window.location = window.location + '#Introduction';
 		window.location.reload();
 	}
